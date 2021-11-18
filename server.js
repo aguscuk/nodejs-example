c60c884c95c086e33a094ea440e26e0839fe8723
aguscuk <aguscuk@gmail.com>
2021-11-18 11:11:30 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Halo apa kabar dunia . . . versi 2');
+  res.send('Halo apa kabar dunia . . . versi 3');
 });
 
 app.listen(PORT, HOST);
